7e1fc91f3333a3157fa6c66bd2e31863dd5042f8
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-10-07 17:37:35 -0500

    color and local stoarge is working

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 var currentDay = moment().format("dddd MMMM DD, YYYY");
-var currentHour = moment().format("H");
+var currentHour = parseInt(moment().format("H"));
 
 var displayDay = function() {
     $("#currentDay").text(currentDay);
+    $("currentHour").currentHour;
    
 };
 
@@ -12,31 +13,35 @@ var displayDay = function() {
 $(document).ready(function () {
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        //  values in the description JQuery
+     debugger
+      //  values in the description JQuery
         var text = $(this).siblings("textarea").val();
         var time = $(this).parent().attr("id");
-      console.log(text);
-        localStorage.setItem(time, JSON.stringify(text));
-    })
-  
-  });
-  localStorage.getItem(text,time)
+      
+        localStorage.setItem(time,text);
+     
+    });
 
-  function background () {
+
+  function background() {
     $(".time-block").each(function(){
+      
       var timeId =parseInt($(this).attr("id"));
       if (timeId <currentHour){
-        $(this).addClass("past");
+        $(this).children("textarea").addClass("past");
       } else if (timeId === currentHour){
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
+        $(this).children("textarea").removeClass("past");
+        $(this).children("textarea").removeClass("future");
+        $(this).children("textarea").addClass("present");
       }else{
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
+        $(this).children("textarea").removeClass("past");
+        $(this).children("textarea").removeClass("present");
+        $(this).children("textarea").addClass("future");
 
       }
+    
     })
+   
   }
   background();
+});
